83c478a7ee925ab7381f8130190a88f7fb659aef
Luciano de Paula <luciano.desenvolvedor@gmail.com>
2019-09-18 12:20:24 -0300

    Alteração irrelevante para o sistema, somente para testes.

--- src/main/java/com/ponto/inteligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/ponto/inteligente/api/PontoInteligenteApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
 
